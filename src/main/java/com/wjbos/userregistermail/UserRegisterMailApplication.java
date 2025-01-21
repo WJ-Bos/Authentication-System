89268691145105ee05f5cd2fd43bf564991c62c8
WJbos <wjbosdev@gmail.com>
2025-01-21 19:10:56 +0200

    initial

---
+++ src/main/java/com/wjbos/userregistermail/UserRegisterMailApplication.java
@@ -0,0 +1,13 @@
+package com.wjbos.userregistermail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegisterMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegisterMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjbos/userregistermail/UserRegisterMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjbos.userregistermail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegisterMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
