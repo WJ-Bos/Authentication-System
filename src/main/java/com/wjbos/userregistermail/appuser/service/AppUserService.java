267678d10f7299d8f90e51a0e99e6f9bd0a9d7e5
WJbos <wjbosdev@gmail.com>
2025-01-21 21:40:00 +0200

    Added a Service for the App user

---
+++ src/main/java/com/wjbos/userregistermail/appuser/service/AppUserService.java
@@ -0,0 +1,24 @@
+package com.wjbos.userregistermail.appuser.service;
+
+import com.wjbos.userregistermail.appuser.model.AppUser;
+import com.wjbos.userregistermail.appuser.repository.AppUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser user = appUserRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("User with email %s not found",email))
+        );
+        return user;
+    }
+}
