5d7e5a8919764aa69e4c24282c262854ed4d40bb
WJbos <wjbosdev@gmail.com>
2025-01-22 19:31:52 +0200

    Added Signup user method

--- src/main/java/com/wjbos/userregistermail/appuser/service/AppUserService.java
+++ src/main/java/com/wjbos/userregistermail/appuser/service/AppUserService.java
@@ -2,17 +2,20 @@ package com.wjbos.userregistermail.appuser.service;
 
 import com.wjbos.userregistermail.appuser.model.AppUser;
 import com.wjbos.userregistermail.appuser.repository.AppUserRepository;
+import com.wjbos.userregistermail.shared.exeptions.EmailAlreadyInUseException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
-    AppUserRepository appUserRepository;
+    private AppUserRepository appUserRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -20,5 +23,18 @@ public class AppUserService implements UserDetailsService {
                 () -> new UsernameNotFoundException(String.format("User with email %s not found",email))
         );
         return user;
+    }
+    public String signUpUser(AppUser user) {
+        boolean userExists = appUserRepository.findByEmail(user.getEmail()).isPresent();
+        if(userExists){
+            throw new EmailAlreadyInUseException(String.format("User with email %s already exists",user.getEmail()));
+        }
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        appUserRepository.save(user);
+        //TODO: Send verify token
+
+        return "";
     }
 }
