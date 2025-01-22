5cb6c572c501dedbad0d701211977a13801f07dc
WJbos <wjbosdev@gmail.com>
2025-01-22 18:39:57 +0200

    changed import

--- src/main/java/com/wjbos/userregistermail/exeptions/NotValidEmailException.java
+++ src/main/java/com/wjbos/userregistermail/exeptions/NotValidEmailException.java
@@ -1,4 +1,4 @@
-package com.wjbos.userregistermail.registration.exeptions;
+package com.wjbos.userregistermail.exeptions;
 
 public class NotValidEmailException extends RuntimeException {
     public NotValidEmailException(String message) {
