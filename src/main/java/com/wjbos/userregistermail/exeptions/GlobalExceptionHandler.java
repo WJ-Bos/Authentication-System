62331820fe00cfb9fd23d6d31fe6e5f878a6faaf
WJbos <wjbosdev@gmail.com>
2025-01-22 18:39:41 +0200

    Added the handler for those cases

--- src/main/java/com/wjbos/userregistermail/exeptions/GlobalExceptionHandler.java
+++ src/main/java/com/wjbos/userregistermail/exeptions/GlobalExceptionHandler.java
@@ -1,14 +1,27 @@
-package com.wjbos.userregistermail.registration.exeptions;
+package com.wjbos.userregistermail.exeptions;
 
 import com.wjbos.userregistermail.registration.response.InvalidEmailResponse;
+import com.wjbos.userregistermail.shared.response.GlobalErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GlobalErrorResponse> handleGlobalException(final Exception ex, final WebRequest request) {
+        GlobalErrorResponse globalErrorResponse = new GlobalErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(globalErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(value = NotValidEmailException.class)
     public ResponseEntity<InvalidEmailResponse> notValidEmailException(NotValidEmailException ex , WebRequest request) {
@@ -18,4 +31,5 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(invalidEmailResponse, HttpStatus.BAD_REQUEST);
     }
+
 }
